f759a90d65419274bca703a1f1c969095793db67
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-12-04 12:25:07 -0300

    feat: editorconfig

---
+++ src/migrations/1638631198852-Users.ts
@@ -0,0 +1,71 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class Users1638631198852 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            length: '36',
+            isNullable: false,
+            isPrimary: true,
+            default: 'uuid()',
+          },
+          {
+            name: 'first_name',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'last_name',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '255',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'datetime',
+            isNullable: false,
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'datetime',
+            isNullable: true,
+          },
+          {
+            name: 'deleted_at',
+            type: 'datetime',
+            isNullable: true,
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
