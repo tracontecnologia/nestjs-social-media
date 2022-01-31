a7efa8899829327629fd4382900f7e95f4ccce71
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2022-01-31 11:45:25 -0300

    feat: install and config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
@@ -9,6 +10,14 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
   app.useStaticAssets(join(__dirname, '..', 'client'), { prefix: '/public' });
+
+  const config = new DocumentBuilder()
+    .setTitle('Social Media API')
+    .setDescription('Essa é a API oficila do Social Media')
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(3333);
 }
