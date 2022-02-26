341d5e6d4b0ec44ac76531d4c4b1fb1a3b707e80
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2022-02-26 10:49:32 -0300

    test(posts): index method

--- src/app/posts/posts.service.spec.ts
+++ src/app/posts/posts.service.spec.ts
@@ -1,18 +1,103 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { IndexPostDto } from './dto/index-post.dto';
+import { PostsEntity } from './entities/posts.entity';
 import { PostsService } from './posts.service';
 
 describe('PostsService', () => {
-  let service: PostsService;
+  let postsService: PostsService;
+  let postsRepository: Repository<PostsEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        {
+          provide: getRepositoryToken(PostsEntity),
+          useValue: {
+            createQueryBuilder: jest.fn().mockReturnThis(),
+            addSelect: jest.fn().mockReturnThis(),
+            leftJoin: jest.fn().mockReturnThis(),
+            andWhere: jest.fn().mockReturnThis(),
+            getMany: jest.fn().mockResolvedValue([]),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<PostsService>(PostsService);
+    postsService = module.get<PostsService>(PostsService);
+    postsRepository = module.get<Repository<PostsEntity>>(getRepositoryToken(PostsEntity));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(postsService).toBeDefined();
+    expect(postsRepository).toBeDefined();
+  });
+
+  describe('index', () => {
+    it('should return a posts list', async () => {
+      // Arrange
+      const options = {};
+      const postsListMock = [
+        {
+          id: '1',
+          userId: '1',
+          description: 'test1',
+          createdAt: '2022-02-26T12:00:00',
+          updatedAt: '2022-02-26T12:00:00',
+          deletedAt: null,
+          user: undefined,
+        },
+        {
+          id: '1',
+          userId: '1',
+          description: 'test1',
+          createdAt: '2022-02-26T12:00:00',
+          updatedAt: '2022-02-26T12:00:00',
+          deletedAt: null,
+          user: undefined,
+        },
+      ];
+      jest.spyOn(postsRepository.createQueryBuilder(), 'getMany').mockResolvedValueOnce(postsListMock);
+      // Act
+      const result = await postsService.index(options);
+      // Assert
+      expect(result).toEqual(postsListMock);
+      expect(result.length).toEqual(postsListMock.length);
+    });
+
+    it('should return an empty posts list', async () => {
+      // Arrange
+      const options = {};
+      const postsListMock = [];
+      jest.spyOn(postsRepository.createQueryBuilder(), 'getMany').mockResolvedValueOnce(postsListMock);
+      // Act
+      const result = await postsService.index(options);
+      // Assert
+      expect(result).toEqual(postsListMock);
+      expect(result.length).toEqual(postsListMock.length);
+    });
+
+    it('should return a posts list by userId', async () => {
+      // Arrange
+      const options: IndexPostDto = { userId: '2' };
+      const postsListMock = [
+        {
+          id: '1',
+          userId: '2',
+          description: 'test1',
+          createdAt: '2022-02-26T12:00:00',
+          updatedAt: '2022-02-26T12:00:00',
+          deletedAt: null,
+          user: undefined,
+        },
+      ];
+      jest.spyOn(postsRepository.createQueryBuilder(), 'getMany').mockResolvedValueOnce(postsListMock);
+      // Act
+      const result = await postsService.index(options);
+      // Assert
+      expect(result).toEqual(postsListMock);
+    });
   });
 });
