5980af4ea5c81745c715b42c24ce6d068fb2dca0
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2022-02-26 12:10:45 -0300

    test(posts): module

---
+++ src/app/posts/posts.module.spec.ts
@@ -0,0 +1,13 @@
+import { PostsModule } from './posts.module';
+
+describe('PostsModule', () => {
+  let postsModule: PostsModule;
+
+  beforeEach(() => {
+    postsModule = new PostsModule();
+  });
+
+  it('should be defined', () => {
+    expect(postsModule).toBeDefined();
+  });
+});
